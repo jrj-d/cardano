98d763264f3fc75547016d8f355bc7501b84ce78
jrj-d <julien.dumazert@gmail.com>
2017-01-07 22:55:27 +0100

    add possibility to specify RandomGenerator

--- src/main/scala/cardano/Stochastic.scala
+++ src/main/scala/cardano/Stochastic.scala
@@ -1,9 +1,10 @@
 package cardano
 
-import breeze.linalg.DenseVector
+import breeze.linalg.{DenseVector, QuasiTensor}
+import breeze.stats.distributions.{Multinomial, RandBasis, ThreadLocalRandomGenerator}
+import org.apache.commons.math3.random.{MersenneTwister, RandomGenerator}
 
 import scala.collection.immutable.IndexedSeq
-import breeze.stats.distributions.Multinomial
 
 trait Stochastic[+A] {
 
@@ -23,23 +24,26 @@ final case class StochasticFlatMap[A, +B](stochastic: Stochastic[A], f: A => Sto
   def sample: B = f(stochastic.sample).sample
 }
 
-final case class StochasticChoose[+A](distribution: Dist[A]) extends Stochastic[A] {
+class StochasticGenerator(randomGenerator: RandomGenerator) {
 
-  lazy val values: IndexedSeq[A] = distribution.map(_._1).toIndexedSeq
-  lazy val sampler: Multinomial[DenseVector[Prob], Int] = Multinomial(DenseVector(distribution.map(_._2).toArray))
+  def choose[A](distribution: Dist[A])(implicit ev: DenseVector[Double] => QuasiTensor[Int, Double], sumImpl: breeze.linalg.sum.Impl[DenseVector[Double], Double]) = new Stochastic[A] {
 
-  def sample: A = values(sampler.sample(1).head)
-}
+    private lazy val values: IndexedSeq[A] = distribution.map(_._1).toIndexedSeq
+    private lazy val sampler: Multinomial[DenseVector[Prob], Int] = Multinomial(DenseVector(distribution.map(_._2).toArray))(ev, sumImpl, new RandBasis(new ThreadLocalRandomGenerator(randomGenerator)))
 
-object Stochastic {
+    def sample: A = values(sampler.sample(1).head)
 
-  def coin(p: Prob = 0.5): StochasticChoose[Int] = StochasticChoose(Seq((0, 1 - p), (1, p)))
-
-  def uniform(n: Int): StochasticChoose[Int] = {
-    val p = 1.0 / n
-    StochasticChoose((0 until n).map((_, p)))
   }
 
-  def fromMass(mass: Iterable[Prob]) = StochasticChoose(mass.zipWithIndex.map{case (p, v) => (v, p)})
+  def coin(p: Prob = 0.5): Stochastic[Int] = choose(Seq((0, 1 - p), (1, p)))
+
+  def uniform(n: Int): Stochastic[Int] = {
+    val p = 1.0 / n
+    choose((0 until n).map((_, p)))
+  }
+
+  def fromMass(mass: Iterable[Prob]) = choose(mass.zipWithIndex.map{case (p, v) => (v, p)})
 
 }
+
+object Stochastic extends StochasticGenerator(new MersenneTwister())
