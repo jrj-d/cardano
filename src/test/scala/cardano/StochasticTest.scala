6576f5dad9aee87fad0647966e6b99f4be1ff50f
jrj-d <julien.dumazert@gmail.com>
2017-01-07 22:55:53 +0100

    add test of expectation using a die throw

--- src/test/scala/cardano/StochasticTest.scala
+++ src/test/scala/cardano/StochasticTest.scala
@@ -1,10 +1,19 @@
 package cardano
 
+import org.apache.commons.math3.random.MersenneTwister
 import org.scalatest._
 
 class StochasticTest extends FlatSpec with Matchers {
 
-  "A dumb test" should "not cause any problem" in {
-    1 shouldBe 1
+  val generator = new StochasticGenerator(new MersenneTwister(0))
+
+  "Expected value of a die throw" should "be around 3.5" in {
+    val die: Stochastic[Int] = generator.uniform(6).map(_ + 1)
+    for(i <- 0 to 100) {
+      die.expectation(10000) should be (3.5 +- 0.1)
+    }
+    for(i <- 0 to 100) {
+      die.expectation(100000) should be (3.5 +- 0.05)
+    }
   }
 }
