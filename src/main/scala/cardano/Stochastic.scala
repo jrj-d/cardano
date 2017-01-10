1ef02372185892faafa3768da58d3608be3764a9
jrj-d <julien.dumazert@gmail.com>
2017-01-10 23:52:37 +0100

    add the higher kind repeat

--- src/main/scala/cardano/Stochastic.scala
+++ src/main/scala/cardano/Stochastic.scala
@@ -3,6 +3,8 @@ package cardano
 import cardano.distributions.AllDistributions
 import org.apache.commons.math3.random.{MersenneTwister, RandomGenerator}
 
+import scala.language.higherKinds
+
 trait Stochastic[+A] {
 
   def map[B](f: A => B): Stochastic[B] = StochasticMap(this, f)
@@ -10,6 +12,8 @@ trait Stochastic[+A] {
   def flatMap[B](f: A => Stochastic[B]): Stochastic[B] = StochasticFlatMap(this, f)
 
   def filter(f: A => Boolean): Stochastic[A] = StochasticFilter(this, f)
+
+  def repeat[B >: A, F[_]](f: (=> B) => F[B]): Stochastic[F[B]] = StochasticHigherKind(this, f)
 
   def sample: A
 
@@ -29,6 +33,11 @@ final case class StochasticFlatMap[A, +B](stochastic: Stochastic[A], f: A => Sto
 
 final case class StochasticFilter[A](stochastic: Stochastic[A], f: A => Boolean) extends Stochastic[A] {
   def sample: A = Stream.continually(stochastic.sample).dropWhile(a => !f(a)).head
+  def randomGenerator: RandomGenerator = stochastic.randomGenerator
+}
+
+final case class StochasticHigherKind[F[_], A](stochastic: Stochastic[A], f: (=> A) => F[A]) extends Stochastic[F[A]] {
+  def sample: F[A] = f(stochastic.sample)
   def randomGenerator: RandomGenerator = stochastic.randomGenerator
 }
 
