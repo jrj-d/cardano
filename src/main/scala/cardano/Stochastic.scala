26d9f770a1b6d50d969e7f59cc3c18a771c37342
jrj-d <julien.dumazert@gmail.com>
2017-01-11 08:40:09 +0100

    add a comment about why repeat feature is not included in map

--- src/main/scala/cardano/Stochastic.scala
+++ src/main/scala/cardano/Stochastic.scala
@@ -37,6 +37,36 @@ final case class StochasticFilter[A](stochastic: Stochastic[A], f: A => Boolean)
 }
 
 final case class StochasticHigherKind[F[_], A](stochastic: Stochastic[A], f: (=> A) => F[A]) extends Stochastic[F[A]] {
+
+  /** This repeat feature could have been implemented using "map".
+    * The only change is replacing `f: A => B` by `f: (=> A) => B`.
+    * This would have work:
+    * {{{
+    *   scala> val die = Stochastic.uniform(6).map(_ + 1)
+    *   die: cardano.Stochastic[Int] = StochasticMap(cardano.distributions.DiscreteDistributions$$anon$1@fc1001b,<function1>)
+    *
+    *   scala> val dice = die.map(Stream.continually[Int])
+    *   dice: cardano.Stochastic[scala.collection.immutable.Stream[Int]] = StochasticMap(StochasticMap(
+    *   cardano.distributions.DiscreteDistributions$$anon$1@fc1001b,<function1>),<function1>)
+    *
+    *   scala> val diePlus5 = die.map(_ + 5)
+    *   diePlus5: cardano.Stochastic[Int] = StochasticMap(StochasticMap(cardano.distributions.DiscreteDistributions$$anon$1@fc1001b,<function1>),<function1>)
+    * }}}
+    * But not:
+    * {{{
+    *   scala> def f(i: Int) = i + 3
+    *   f: (i: Int)Int
+    *
+    *   scala> die.map(f)
+    *   <console>:17: error: type mismatch;
+    *     found   : Int => Int
+    *     required: (=> Int) => ?
+    *        die.map(f)
+    * }}}
+    * Since it is not reasonable to ask the user to put `(=> A)` in all their signatures for something as basic as `map`,
+    * it is better to separate the two functionalities
+    */
+
   def sample: F[A] = f(stochastic.sample)
   def randomGenerator: RandomGenerator = stochastic.randomGenerator
 }
