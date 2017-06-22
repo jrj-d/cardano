f84ed02b4ced4d52ec90dea60c7d9befd2f4d3bb
jrj-d <julien.dumazert@gmail.com>
2017-06-22 20:09:49 +0200

    add tests about monadic structure and tail recursion

--- src/test/scala/cardano/ModelTest.scala
+++ src/test/scala/cardano/ModelTest.scala
@@ -3,15 +3,13 @@ package cardano
 import org.scalatest._
 import cardano.inference.prior
 import org.apache.commons.math3.random.{MersenneTwister, RandomGenerator}
-import cardano.semifield.syntax._
+import cardano.semifield.logprob._
 
 import scala.annotation.tailrec
 
 class ModelTest extends FlatSpec with Matchers {
 
   "Model prior and Stochastic" should "output same samples (1)" in {
-
-    import cardano.semifield.logprob._
 
     val stochastic: Stochastic[Double] = Stochastic.gaussian(3.0, 2.0).map(_ > 2.5)
         .flatMap { b => if(b) Stochastic.beta(3.0, 5.0) else Stochastic.pure(0.8) }
@@ -29,8 +27,6 @@ class ModelTest extends FlatSpec with Matchers {
 
   "Model prior and Stochastic" should "output same samples (2)" in {
 
-    import cardano.semifield.logprob._
-
     val stochastic: Stochastic[Double] = Stochastic.gaussian(3.0, 2.0)
 
     val model: Model[Double] = Stochastic.gaussian(3.0, 2.0).l
@@ -45,8 +41,6 @@ class ModelTest extends FlatSpec with Matchers {
 
   "Model prior and Stochastic" should "output same samples (3)" in {
 
-    import cardano.semifield.logprob._
-
     val stochastic: Stochastic[Double] = Stochastic.pure(3.0).flatMap(a => Stochastic.gaussian(a, 2.0))
 
     val model: Model[Double] = Model.pure(3.0).flatMap(a => Stochastic.gaussian(a, 2.0).l)
@@ -60,8 +54,6 @@ class ModelTest extends FlatSpec with Matchers {
   }
 
   "Model prior and Stochastic" should "output same samples (4)" in {
-
-    import cardano.semifield.logprob._
 
     val stochastic: Stochastic[Double] = Stochastic.gaussian(3.0, 2.0)
       .flatMap(a => Stochastic.gaussian(a, 2.0))
@@ -81,8 +73,6 @@ class ModelTest extends FlatSpec with Matchers {
 
   "Model prior and Stochastic" should "output same samples (5)" in {
 
-    import cardano.semifield.logprob._
-
     val stochastic: Stochastic[Double] = Stochastic.gaussian(3.0, 2.0)
       .flatMap(a => Stochastic.gaussian(a, 2.0))
 
@@ -100,8 +90,6 @@ class ModelTest extends FlatSpec with Matchers {
 
   "Model" should "not support consecutive weights" in {
 
-    import cardano.semifield.logprob._
-
     implicit val random = new MersenneTwister(42)
 
     a [RuntimeException] should be thrownBy {
@@ -113,21 +101,29 @@ class ModelTest extends FlatSpec with Matchers {
     }
   }
 
+  @tailrec final def loop(acc: StringBuilder, m: Model[Int])(implicit random: RandomGenerator): Int = {
+    acc += '|'
+    m.step match {
+      case Left(s) =>
+        val (_, newModel) = s.sample
+        loop(acc, newModel)
+      case Right((_, a)) => a
+    }
+  }
+
+  def stochasticMarker(i: Int, acc: StringBuilder): Model[Int] = Stochastic { (_: RandomGenerator) =>
+    acc ++= s"s$i"
+    i
+  }.l
+
+  def weightMarker(acc: StringBuilder)(i: Int): Double = {
+    acc ++= s"w$i"
+    0.0
+  }
+
   "Weights" should "be applied at the right time" in {
 
-    import cardano.semifield.logprob._
-
     implicit val random = new MersenneTwister(42)
-
-    def stochasticMarker(i: Int, acc: StringBuilder): Model[Int] = Stochastic { (_: RandomGenerator) =>
-      acc ++= s"s$i"
-      i
-    }.l
-
-    def weightMarker(acc: StringBuilder)(i: Int): Double = {
-      acc ++= s"w$i"
-      0.0
-    }
 
     val acc = new StringBuilder
 
@@ -137,19 +133,41 @@ class ModelTest extends FlatSpec with Matchers {
       k <- stochasticMarker(i + j, acc).weight(weightMarker(acc))
     } yield k
 
-    @tailrec def loop(m: Model[Int]): Int = {
-      acc += '|'
-      m.step match {
-        case Left(s) =>
-          val (_, newModel) = s.sample
-          loop(newModel)
-        case Right((_, a)) => a
-      }
-    }
-
-    loop(model)
+    loop(acc, model)
 
     acc.toString shouldEqual "|s3w3|s4w4|s7w7|"
+  }
+
+  "Weighting" should "not change a thing when applied left or right hand side" in {
+
+    implicit val random = new MersenneTwister(42)
+
+    val acc1 = new StringBuilder
+    val model1: Model[Int] = stochasticMarker(1, acc1)
+      .flatMap(i => stochasticMarker(i + 1, acc1))
+      .weight(weightMarker(acc1))
+    loop(acc1, model1)
+
+    val acc2 = new StringBuilder
+    val model2: Model[Int] = stochasticMarker(1, acc2)
+      .flatMap(i => stochasticMarker(i + 1, acc2))
+      .weight(weightMarker(acc2))
+    loop(acc2, model2)
+
+    acc1.toString shouldEqual "|s1|s2w2|"
+    acc2.toString shouldEqual "|s1|s2w2|"
+  }
+
+  "Model" should "not cause stack overflows (contrary to Stochastic)" in {
+
+    implicit val random = new MersenneTwister(42)
+
+    def loopM(acc: Model[Double], n: Int): Model[Double] = n match {
+      case 0 => acc
+      case i => loopM(acc.flatMap(a => Stochastic.gaussian.l.map(a + _)), i - 1)
+    }
+
+    prior(loopM(Model.pure(0.0), 100000)).sample
   }
 
 }
